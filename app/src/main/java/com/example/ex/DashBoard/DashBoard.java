f805c6a884727cef68781d634cce5258bfd7abf4
Infocar <dkwkrhrh07@naver.com>
2022-09-20 18:01:59 +0900

    2022/0920

--- app/src/main/java/com/example/ex/DashBoard/DashBoard.java
+++ app/src/main/java/com/example/ex/DashBoard/DashBoard.java
@@ -5,23 +5,33 @@ import static androidx.constraintlayout.motion.utils.Oscillator.TAG;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.motion.utils.Easing;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Picture;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.ex.MainActivity.MainActivity;
 import com.example.ex.R;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
@@ -31,6 +41,8 @@ import java.util.Objects;
 import java.util.Random;
 
 import at.grabner.circleprogress.CircleProgressView;
+import az.plainpie.PieView;
+import az.plainpie.animation.PieAngleAnimation;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -38,8 +50,7 @@ public class DashBoard extends AppCompatActivity {
     private static final float MAX_Y_VALUE = 4;
     private static final float MIN_Y_VALUE = 1;
     private static final String SET_LABEL = "dash";
-
-    public CircleProgressView SpeedView,RpmView;
+    public PieChart pieChart;
     String TAG = "DashBoard_Activity";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -48,9 +59,6 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-        SpeedView = findViewById(R.id.Speed_CPB);
-        RpmView = findViewById(R.id.RPM_CPB);
-
         //-------------------
         Toolbar toolbar = findViewById(R.id.DashBoard_toolbar);
         setSupportActionBar(toolbar);
@@ -58,12 +66,21 @@ public class DashBoard extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //-------------------
 
-        SpeedView.setTextColorAuto(true);
-        SpeedView.setText("22");
-        RpmView.setText("23145");
-        RpmView.setTextColorAuto(true);
+        PieView pieView = findViewById(R.id.pieView);
+        pieView.setPercentageBackgroundColor(R.color.purple_200);
+
+        pieView.setPieInnerPadding(30);
+
+        // Update the visibility of the widget text
+        pieView.setInnerTextVisibility(View.VISIBLE); // 안에 텍스트 보여주기
+        pieView.setPercentage(33);
+        pieView.setInnerText("속도"); // 안에 텍스트 변경
+        pieView.setPercentageTextSize(35); //안에 텍스트 사이즈
+
+
 
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { // 뒤로가기 버튼 만들고 누르면 작동하는 함수..
